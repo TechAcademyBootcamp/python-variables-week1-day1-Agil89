ac8987891afb6c56fa8afadddc29f87c98ec6cfe
Agil89 <agil.makhmudov@mail.ru>
2020-05-19 18:42:41 +0400

    16 divis

---
+++ 16-Divisibility-by-numbers/main.py
@@ -0,0 +1,8 @@
+n = int(input('enter the number n:'))
+a = int(input('enter the number a:'))
+b = int(input('enter the number b:'))
+
+if n%a==0 and n%b ==0:
+    print('YES')
+else:
+    print('NO')
