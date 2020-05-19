8abd3fc65bca1bf44e3499f71989566cb8dfb23b
Agil89 <agil.makhmudov@mail.ru>
2020-05-19 16:18:49 +0400

    06 apples

---
+++ 06-Divide-The-Apples/main.py
@@ -0,0 +1,7 @@
+n = int(input('number of childs:'))
+while n>1500:
+    print('enter the number less than 1500')
+    n = input('number of childs:')
+k = int(input('number of apples:'))
+
+print(k//n)
